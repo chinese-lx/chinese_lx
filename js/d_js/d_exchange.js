1714b35fc952a4a4af8790da1f4808a588c0fa50
eason35 <403445363@qq.com>
2019-07-01 21:25:05 +0800

    完善兑换中心页面底部动态加载，更新兑换详情页面

--- js/d_js/d_exchange.js
+++ js/d_js/d_exchange.js
@@ -3,7 +3,7 @@ $(function () {
     $.ajax({
         type: 'post',
         dataType: 'json',
-        url: '../data/exchange_ledou.json',
+        url: '../data/d_exchange_ledou.json',
         success: function (json) {
             var result = '';
             $.each(json, function (index, value) {
@@ -15,7 +15,7 @@ $(function () {
                 }
             });    
             if(json.length>10){
-                var all = result + '<div class="d_moregoods"><p>查看更多商品</p><div><img src="../img/d_img/d_morearr.png"></div></div>';
+                var all = result + '<div class="d_moregoods d_ledou"><p>查看更多商品</p><div><img src="../img/d_img/d_morearr.png"></div></div>';
             }else{
                 var all = result + '<div class="d_moregoods"><p>没有了</p><div><img src="../img/d_img/d_morearr.png"></div></div>';
             }
@@ -23,14 +23,14 @@ $(function () {
         }
     
     })
-});
+
 //积分请求
 //去d_data请求数据
-$(function () {
+
     $.ajax({
         type: 'post',
         dataType: 'json',
-        url: '../data/exchange_jifen.json',
+        url: '../data/d_exchange_jifen.json',
         success: function (json) {
             var result = '';
             $.each(json, function (index, value) {
@@ -49,8 +49,59 @@ $(function () {
             $('.d_jifenbox').html(all);
         }
     
-    })
+    });
+    var count = 2;
+    //更多商品的点击事件 点击了之后再拉去十条数据
+    $('.d_listbox').on('click','.d_ledou',function(){
+        console.log('执行到事件');
+         $.ajax({
+            type: 'get',
+            dataType: 'json',
+            url: '../data/d_exchange_ledou.json',
+            success:function(json){
+                console.log('执行ajax');
+               if($('.d_ledou').attr('count')){
+                     var  data='';
+                    //如果这个属性存在就读出来是第几次请求
+                    $.each(json, function(index, value) {
+                        if(index>=10*$('.d_ledou').attr('count')-10 && index<10*$('.d_ledou').attr('count')){
+                            console.log('执行if');
+                            data += '<div class="d_list"><div class="d_img"><img src="' + value.path +
+                         '" alt=""></div><div class="d_detailbox"><p class="d_details">' + value.details + 
+                         '</p></div><p class="d_price"><span class="wordorange">' + value.price +
+                          '</span>+<span class="wordorange">' + value.virtual + '</span>乐豆</p></div>';
+                        }
+                    });
+                    $('.d_ledou').before(data);
+                    if(10*$('.d_ledou').attr('count')>json.length){
+                        $('.d_ledou').find('p').html('没有了');
+                        return;
+                    }
+                    $('.d_ledou').attr('count',parseInt($('.d_ledou').attr('count'))+1);
+               }else{
+                   //否则肯定是第二次 加载完数据之后  加count=3
+                   var result = '';
+                   $.each(json, function (index, value) {
+                       if(index>=10 && index<20){
+                           result += '<div class="d_list"><div class="d_img"><img src="' + value.path +
+                        '" alt=""></div><div class="d_detailbox"><p class="d_details">' + value.details + 
+                        '</p></div><p class="d_price"><span class="wordorange">' + value.price +
+                         '</span>+<span class="wordorange">' + value.virtual + '</span>乐豆</p></div>';
+                       }
+                   });
+                   $('.d_ledou').before(result);
+                   if(json.length<20){
+                        $('.d_ledou').find('p').html('没有了');
+                        $('.d_listbox').off('click');
+                        return;
+                   }
+                   $('.d_ledou').attr('count','3');
+               }
+            }
+         });
+    });
 });
+
 
 
 
--- js/d_js/itemgoods.js
+++ js/d_js/itemgoods.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
-
+    //购买数量加减 ，价格和乐豆随之变化 
+    //单价
+    var singleprice = $('.d_trueprice').html();
+    var singleledou = $('.d_bigledou').html();
     $('.d_reduce').bind('click',function(){
         
         if($('.d_count').html()==1){
             $('.d_count').html("1");
+            $('.d_trueprice').html(singleprice);
+            $('.d_bigledou').html(singleledou);
         }else{
             $('.d_count').html($('.d_count').html()-1);
             // var p = $('.d_count').html();
             // $('.d_price d_trueprice').html();
+            //价格
+            $('.d_trueprice').html(singleprice*$('.d_count').html());
+            $('.d_bigledou').html(singleledou*$('.d_count').html());
         }
 
     });
@@ -16,10 +24,14 @@ $(document).ready(function(){
         
         if($('.d_count').html()==5){
             $('.d_count').html("5");
+            $('.d_trueprice').html(singleprice*5);
+            $('.d_bigledou').html(singleledou*5);
         }else{
             $('.d_count').html(parseInt($('.d_count').html())+1);
             // var p = $('.d_count').html();
             // $('.d_price d_trueprice').html(parseInt(p)*$('.d_price d_trueprice').html());
+            $('.d_trueprice').html(singleprice*$('.d_count').html());
+            $('.d_bigledou').html(singleledou*$('.d_count').html());
         }
     });
     
@@ -70,4 +82,59 @@ $(document).ready(function(){
     $('.d_introduction').find('i').mouseout(function(){
         $('.d_smallintro').css('display','none');
    });
-})
+
+   //动画
+    $('.d_bgred').hover(function(){
+        $('.d_showmsg').slideDown({
+            duration:500,
+            easing:'linear'
+        });
+        $('.d_showwords').html($('.d_heading').html()+'<br>￥'+$('.d_trueprice').html()+'+'+$('.d_bigledou').html()+'乐豆');
+    },function(){
+        $('.d_showmsg').slideUp(500,'linear');
+    });
+
+    //小图区mouseover事件
+    $('.d_imgsmall').on('mouseover','li',function(){
+        //当前边框变红
+        $(this).addClass('d_imgchecked').siblings().removeClass('d_imgchecked');
+        $('.d_bigimg').find('img').attr('src','../img/d_img/itemgoods/d_big'+$(this).attr('index')+'.jpg');
+        $('.d_bigimg').attr('index',$(this).attr('index'));
+        if($(this).attr('index')==1){
+            $('.d_leftarr').css('opacity','0.3');
+        }else{
+            $('.d_leftarr').css('opacity','1');
+        }
+    });
+    //小图区箭头点击事件
+    //左箭头  点击箭头，当前的
+    $('.d_leftarr').click(function(){
+        //主要：找到当前的红框li
+        if($('.d_bigimg').attr('index')==1){
+            $(this).css('opacity','0.3');
+        }else{
+            $(this).css('opacity','1');
+            $('.d_imgsmall').find('li[index='+$(".d_bigimg").attr("index")+']').prev().addClass('d_imgchecked').siblings().removeClass('d_imgchecked');
+            var newIndex = $('.d_bigimg').attr('index')-1;
+            if(newIndex==1){
+                $(this).css('opacity','0.3');
+            }
+            $('.d_bigimg').attr('index',newIndex);
+            $('.d_bigimg').find('img').attr('src','../img/d_img/itemgoods/d_big'+newIndex+'.jpg'); 
+        }
+    });
+    $('.d_rightarr').click(function(){
+        //当到了第五张图时
+        if($('.d_bigimg').attr('index')==5){
+            
+        }else{
+            $('.d_imgsmall').find('li[index='+$(".d_bigimg").attr("index")+']').next().addClass('d_imgchecked').siblings().removeClass('d_imgchecked');
+            var newIndex = parseInt($('.d_bigimg').attr('index'))+1;
+            $('.d_bigimg').attr('index',newIndex);
+            $('.d_bigimg').find('img').attr('src','../img/d_img/itemgoods/d_big'+newIndex+'.jpg'); 
+        }
+    });
+
+    
+
+});
